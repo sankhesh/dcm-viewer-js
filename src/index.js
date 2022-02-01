e823bd87c5d111ca2cddda4827f27ec303d6c78c
Sankhesh Jhaveri <sankhesh.jhaveri@kitware.com>
2022-01-31 20:12:47 -0500

    WIP: Debug memory leak with reading DICOM files

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
-import readImageDICOMFileSeries from 'itk/readImageDICOMFileSeries';
-import Viewer from './viewer';
+import { readImageDICOMFileSeries } from "itk-wasm/dist/browser/index.js";
+import Viewer from "./viewer";
 
 // Setup viewer with DOM
-const viewerContainer = document.getElementById('viewer');
-const topLabel = document.getElementById('top');
-const bottomLabel = document.getElementById('bottom');
-const leftLabel = document.getElementById('left');
-const rightLabel = document.getElementById('right');
-const viewer = new Viewer(
-  viewerContainer,
-  { topLabel, bottomLabel, leftLabel, rightLabel },
-);
+const viewerContainer = document.getElementById("viewer");
+const topLabel = document.getElementById("top");
+const bottomLabel = document.getElementById("bottom");
+const leftLabel = document.getElementById("left");
+const rightLabel = document.getElementById("right");
+const viewer = new Viewer(viewerContainer, {
+  topLabel,
+  bottomLabel,
+  leftLabel,
+  rightLabel,
+});
 
 // Files
-const fileInput = document.querySelector('input');
-fileInput.addEventListener('change', async (event) => {
-
+const fileInput = document.querySelector("input");
+fileInput.addEventListener("change", async (event) => {
   // Get files
   const { dataTransfer } = event;
   const files = event.target.files || dataTransfer.files;
@@ -23,14 +24,53 @@ fileInput.addEventListener('change', async (event) => {
   fileInput.setAttribute("hidden", "");
 
   // Read series
-  const itkReader = await readImageDICOMFileSeries(files)
+  let itkReader = await readImageDICOMFileSeries(files);
+
+  if (itkReader.webWorkerPool) {
+    const killMe = itkReader.webWorkerPool.workerQueue.filter(
+      (x) => x !== null
+    );
+    console.log(killMe);
+    killMe.forEach((x) => {
+      console.log(x);
+      x.terminate();
+    });
+    itkReader.webWorkerPool.terminateWorkers();
+  }
+//  const get = async () => {
+//    console.log("getting");
+//    let itkReader = await readImageDICOMFileSeries(files);
+//
+//    console.log(itkReader);
+//    if (itkReader.webWorkerPool) {
+//      console.log("killing workers");
+//      const killMe = itkReader.webWorkerPool.workerQueue.filter(
+//        (x) => x !== null
+//      );
+//      killMe.forEach((x) => {
+//        console.log(x);
+//        x.terminate();
+//      });
+//      itkReader.webWorkerPool.workerQueue = Array(16).fill(null);
+//    }
+//    // get();
+//  };
+  // get();
+
+  // setInterval(() => {
+  //   get();
+  // }, 1000);
+
+  itkReader.image.imageType.pixelType = 1;
+  itkReader.image.direction.data = itkReader.image.direction;
 
   // Load in viewer
-  const selector = document.getElementById('slicingModeSelector')
+  const selector = document.getElementById("slicingModeSelector");
   viewer.load(itkReader.image, parseInt(selector.value));
 
   selector.removeAttribute("hidden");
   selector.onchange = () => {
     viewer.load(itkReader.image, parseInt(selector.value));
-  }
+  };
+  // get(files);
 });
