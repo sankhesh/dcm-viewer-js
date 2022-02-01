aa008a4a909b00d9734bd5e8b14aa81d33177ccb
Sankhesh Jhaveri <sankhesh.jhaveri@kitware.com>
2022-01-31 20:12:12 -0500

    Switch to itk-wasm

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,6 @@ const path = require('path');
 const entry = path.join(__dirname, 'src', 'index.js');
 const html = path.join(__dirname, 'src', 'index.html');
 const style = path.join(__dirname, 'src', 'style.css');
-const itkWebWorkers = path.join('itk', 'WebWorkers', 'ImageIO.worker.js');
-const itkImageIOs = path.join('itk', 'ImageIOs');
 const nodeModules = path.join(__dirname, 'node_modules');
 const dist = path.join(__dirname, 'dist');
 
@@ -25,18 +23,6 @@ module.exports = {
         {
           from: style,
           to: dist,
-        },
-        {
-          from: path.join(nodeModules, itkWebWorkers),
-          to: path.join(dist, itkWebWorkers),
-        },
-        {
-          from: path.join(nodeModules, itkImageIOs, '*DICOM*'),
-          to: path.join(dist, itkImageIOs, '[name][ext]'),
-        },
-        {
-          from: path.join(nodeModules, itkImageIOs, '*GDCM*'),
-          to: path.join(dist, itkImageIOs, '[name][ext]'),
         },
       ],
     }),
